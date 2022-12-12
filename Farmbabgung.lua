ca3b3cc95a55dfe0a635c957800387aa2c830be3
FanZ <95639655+Okumon@users.noreply.github.com>
2022-12-12 09:41:30 +0700

    Add files via upload

--- Farmbabgung.lua
+++ Farmbabgung.lua
@@ -1,3 +1,160 @@
+shared.LoaderTitle = 'Reskin Hub Load✅' -- ใส่ชื่อ
+shared.LoaderKeyFrames = {
+   [1] = {1, 30}, -- [Time (s), Percentage] 
+   [2] = {3, 100} -- [เวลา, เปอร์เซ็น]
+}
+
+local Metadata = {
+	LoaderData = {
+		Name = (shared.LoaderTitle or 'A Loader'),
+		Colors = shared.LoaderColors or {
+			Main = Color3.fromRGB(24, 24, 24),
+			Topic = Color3.fromRGB(0, 135, 251),
+			Title = Color3.fromRGB(0, 135, 251),
+			LoaderBackground = Color3.fromRGB(30, 30, 30),
+			LoaderSplash = Color3.fromRGB(255, 255, 255)
+		}
+	},
+	Keyframes = shared.LoaderKeyFrames or {
+		[1] = {1, 10}, -- [Time (s), Percentage]
+		[2] = {2, 30},
+		[3] = {3, 60},
+		[4] = {2, 100}
+	}
+}
+
+--
+local function tweenObject(object, speed, info)
+	game.TweenService:Create(object, TweenInfo.new(speed, Enum.EasingStyle.Linear, Enum.EasingDirection.InOut), info):Play()
+end
+local function createObject(className, properties)
+	local instance = Instance.new(className)
+	local parent
+	for propertyName, propertyValue in pairs(properties) do
+		if propertyName ~= "Parent" then
+			instance[propertyName] = propertyValue
+		else
+			parent = propertyValue
+		end
+	end
+	instance.Parent = parent
+	return instance
+end
+
+
+local Core = createObject("ScreenGui", {
+	Name = "Core",
+	Parent = game.CoreGui
+})
+local Main = createObject("Frame", {
+	Name = "Main",
+	Parent = Core,
+	BackgroundColor3 = Metadata.LoaderData.Colors.Main,
+	BorderSizePixel = 0,
+	ClipsDescendants = true,
+	Position = UDim2.new(0.5, 0, 0.5, 0),
+	AnchorPoint = Vector2.new(0.5, 0.5),
+	Size = UDim2.new(0, 0, 0, 0),
+})
+local Top = createObject("TextLabel", {
+	Name = "Top",
+	TextTransparency = 1,
+	Parent = Main,
+	BackgroundColor3 = Color3.fromRGB(255, 255, 255),
+	BackgroundTransparency = 1,
+	Position = UDim2.new(0, 30, 0, 8),
+	Size = UDim2.new(0, 301, 0, 50),
+	Font = Enum.Font.Gotham,
+	Text = "Loader",
+	TextColor3 = Metadata.LoaderData.Colors.Topic,
+	TextSize = 10,
+	TextXAlignment = Enum.TextXAlignment.Left,
+})
+local Title = createObject("TextLabel", {
+	Name = "Title",
+	Parent = Main,
+	TextTransparency = 1,
+	BackgroundColor3 = Color3.fromRGB(255, 255, 255),
+	BackgroundTransparency = 1,
+	Position = UDim2.new(0, 30, 0, 27),
+	Size = UDim2.new(0, 301, 0, 46),
+	Font = Enum.Font.Gotham,
+	RichText = true,
+	Text = "<b>" .. Metadata.LoaderData.Name .. "</b>",
+	TextColor3 = Metadata.LoaderData.Colors.Title,
+	TextSize = 14,
+	TextXAlignment = Enum.TextXAlignment.Left,
+})
+local BG = createObject("Frame", {
+	Name = "BG",
+	Parent = Main,
+	AnchorPoint = Vector2.new(0.5, 0),
+	BackgroundTransparency = 1,
+	BackgroundColor3 = Metadata.LoaderData.Colors.LoaderBackground,
+	BorderSizePixel = 0,
+	Position = UDim2.new(0.5, 0, 0, 70),
+	Size = UDim2.new(0.8500000238418579, 0, 0, 24),
+})
+local Progress = createObject("Frame", {
+	Name = "Progress",
+	Parent = BG,
+	BackgroundColor3 = Metadata.LoaderData.Colors.LoaderSplash,
+	BackgroundTransparency = 1,
+	BorderSizePixel = 0,
+	Size = UDim2.new(0, 0, 0, 24),
+})
+
+local function updatePercentage(percentage)
+	tweenObject(Progress, 0.5, {
+		Size = UDim2.new((percentage / 100), 0, 0, 24)
+	})
+end
+
+
+-- Loader itself
+tweenObject(Main, 0.25, {
+	Size = UDim2.new(0, 346, 0, 121)
+})
+wait(0.25)
+tweenObject(Top, 0.5, {
+	TextTransparency = 0
+})
+tweenObject(Title, 0.5, {
+	TextTransparency = 0
+})
+tweenObject(BG, 0.5, {
+	BackgroundTransparency = 0
+})
+tweenObject(Progress, 0.5, {
+	BackgroundTransparency = 0
+})
+
+for i, v in pairs(Metadata.Keyframes) do
+	wait(v[1]);
+	updatePercentage(v[2])
+end
+updatePercentage(100)
+
+tweenObject(Top, 0.5, {
+	TextTransparency = 1
+})
+tweenObject(Title, 0.5, {
+	TextTransparency = 1
+})
+tweenObject(BG, 0.5, {
+	BackgroundTransparency = 1
+})
+tweenObject(Progress, 0.5, {
+	BackgroundTransparency = 1
+})
+wait(0.5)
+tweenObject(Main, 0.25, {
+	Size = UDim2.new(0, 0, 0, 0)
+})
+wait(0.25);
+Core:Destroy()
+
+
 _G.Setting_table = {
    BringMonster = true,
    FastAttack = true,
@@ -86,7 +243,7 @@ function Com()
     })
 end
 
-_G.Color = Color3.fromRGB(255,0,0)
+_G.Color = Color3.fromRGB(146,132,240)
 repeat wait(1) until game:IsLoaded()
 
 IKAI = true
@@ -1694,7 +1851,7 @@ end)
 UICorner.Parent = ImageButton
 
 
-local win = library:Window("    PlayBack X ",[[     Hub | BF]],[[Ver:4.1]],"10729012702",Enum.KeyCode.RightControl)
+local win = library:Window("    Reskin X ",[[     Hub | BF]],[[Ver:0.1]],"10729012702",Enum.KeyCode.RightControl)
 local Tap = win:Tab("Main Farm",[[7040391851]])
 local Stats = win:Tab("Stats",[[7040410130]])
 local Combat = win:Tab("Combat",[[9606626034]])
@@ -1744,12 +1901,19 @@ coroutine.wrap(function()
     end
     end)
     end)
+    
+    
+ 
+
+
+
 
 Tap:Toggle("BringMonster","6022668898",_G.Setting_table.BringMonster,function(t)
       BringMonster = t
       _G.Setting_table.BringMonster = t
      Update_Setting(getgenv()['MyName'])
 end)
+
 
 FarmMod = {
 	"Normal",
@@ -1810,6 +1974,13 @@ Tap:Button("Refresh Weapon",function(vu)
 	end
 end)
 
+
+
+
+
+
+
+
 Tap:Seperator("FarmMastery")
 
 Tap:Toggle("Auto Farm FruitMastery","6022668898",_G.Setting_table.AutoFarmMastery,function(t)
@@ -1824,8 +1995,8 @@ Tap:Toggle("Auto Farm MasteryGun","6022668898",_G.Setting_table.AutoFarmMasteryG
      Update_Setting(getgenv()['MyName'])
 end)
 
-_G.KillAt = 70
-Tap:Slider("Select HealthMon",1,100,70,function(value)
+_G.KillAt = 35
+Tap:Slider("Select HealthMon",1,100,35,function(value)
     _G.KillAt = value
 end)
 
@@ -1847,6 +2018,8 @@ task.spawn(function()
                                 EquipWapon(_G.Setting_table.Weapon)
                                 StartAutoFarmMastery = true
                                 BringBone()
+                                game:GetService'VirtualUser':CaptureController()
+                                game:GetService'VirtualUser':Button1Down(Vector2.new(1280, 672))
                                 v.HumanoidRootPart.CanCollide = false
                                 v.Humanoid.WalkSpeed = 0
                                 v.Head.CanCollide = false
@@ -3588,6 +3761,8 @@ local TimeRaid = Dungeon:Label("Wait For Dungeon")
         end)
     end
 end
+
+
 ----------------------------Shop----------------------------
 Shop:Seperator("Abilities")
 
@@ -3955,7 +4130,185 @@ Misc:Toggle('Auto Rejoin',"6022668898",true,function(value)
 	            end
 	        end
       end)
+      
+Misc:Button("Cap kaitun",function()
+	local cac = require(game:GetService("Players").LocalPlayer.PlayerGui.Main.UIController.Inventory)
+		local Inventory = game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("getInventory")
+		local Items = {}
+		local RaityLevel = {"Mythical","Legendary","Rare","Uncommon","Common"}
+		local RaityColor =  {
+			["Common"] = Color3.fromRGB(179, 179, 179),
+			["Uncommon"] = Color3.fromRGB(92, 140, 211),
+			["Rare"] =  Color3.fromRGB(140, 82, 255),
+			["Legendary"] = Color3.fromRGB(213, 43, 228) ,
+			["Mythical"] =  Color3.fromRGB(238, 47, 50)
+		}
+		function GetRaity(color)
+			for k,v in pairs(RaityColor) do 
+				if v==color then return k end
+			end
+		end
+
+		for k,v in pairs(Inventory) do 
+			Items[v.Name] = v
+		end
+
+		local total = #getupvalue(cac.UpdateRender,4)
+		local rac = {}
+		local allitem = {}
+		local total2 = 0
+		while total2<total do 
+			local i = 0
+			while i < 25000 and total2<total do 
+				game:GetService("Players").LocalPlayer.PlayerGui.Main.InventoryContainer.Right.Content.ScrollingFrame.CanvasPosition = Vector2.new(0,i)
+				for k,v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Main.InventoryContainer.Right.Content.ScrollingFrame.Frame:GetChildren()) do 
+					if v:IsA("Frame") and not rac[v.ItemName.Text] and v.ItemName.Visible==true then 
+						local vaihuhu = GetRaity(v.Background.BackgroundColor3)
+						if vaihuhu then
+							print("Rac")
+							if not allitem[vaihuhu] then 
+								allitem[vaihuhu] = {}
+							end
+							table.insert(allitem[vaihuhu],v:Clone())
+						end
+						total2=total2+1
+						rac[v.ItemName.Text] = true
+					end
+				end
+				i=i+20
+			end
+			wait()
+		end
+		function GetXY(vec) 
+			return vec*100
+		end
+
+		local tvk = Instance.new("UIListLayout")
+		tvk.FillDirection = Enum.FillDirection.Vertical
+		tvk.SortOrder = 2
+		tvk.Padding = UDim.new(0,10)
+
+		local Left = Instance.new("Frame",game.Players.LocalPlayer.PlayerGui.BubbleChat)
+		Left.BackgroundTransparency = 1
+		Left.Size = UDim2.new(.5,0,1,0) 
+		tvk.Parent = Left
+
+		local Right = Instance.new("Frame",game.Players.LocalPlayer.PlayerGui.BubbleChat)
+		Right.BackgroundTransparency = 1
+		Right.Size = UDim2.new(.5,0,1,0) 
+		Right.Position = UDim2.new(.6,0,0,0)
+		tvk:Clone().Parent = Right
+		for k,v in pairs(allitem) do 
+			local cac = Instance.new("Frame",Left)
+			cac.BackgroundTransparency = 1
+			cac.Size = UDim2.new(1,0,0,0) 
+			cac.LayoutOrder = table.find(RaityLevel,k)
+
+			local cac2 = Instance.new("Frame",Right)
+			cac2.BackgroundTransparency = 1
+			cac2.Size = UDim2.new(1,0,0,0) 
+			cac2.LayoutOrder = table.find(RaityLevel,k)
+
+			local tvk = Instance.new("UIGridLayout",cac)
+			tvk.CellPadding = UDim2.new(.005,0,.005,0)
+			tvk.CellSize =  UDim2.new(0,70,0,70)
+			tvk.FillDirectionMaxCells = 100
+			tvk.FillDirection = Enum.FillDirection.Horizontal
+
+			local ccc = tvk:Clone()
+			ccc.Parent = cac2
+			for k,v in pairs(v) do 
+				if Items[v.ItemName.Text] and Items[v.ItemName.Text].Mastery then 
+					if v.ItemLine2.Text~="Accessory" then 
+						local bucac = v.ItemName:Clone()
+						bucac.BackgroundTransparency = 1
+						bucac.TextSize = 10
+						bucac.TextXAlignment  = 2
+						bucac.TextYAlignment  = 2
+						bucac.ZIndex  = 5
+						bucac.Text = Items[v.ItemName.Text].Mastery
+						bucac.Size = UDim2.new(.5,0,.5,0)
+						bucac.Position = UDim2.new(.5,0,.5,0)
+						bucac.Parent = v
+					end
+					v.Parent = cac
+				elseif v.ItemLine2.Text == "Blox Fruit" then 
+					v.Parent = cac2
+				end
+			end
+			cac.AutomaticSize = 2
+			cac2.AutomaticSize = 2
+		end
+		local ListHuhu = {
+			["Superhuman"] = Vector2.new(3,2),
+			["DeathStep"] = Vector2.new(4,3),
+			["ElectricClaw"] = Vector2.new(2,0),
+			["SharkmanKarate"] = Vector2.new(0,0),
+			["DragonTalon"] = Vector2.new(1,5)
+		}
+		local MeleeG = Instance.new("Frame",Left)
+		MeleeG.BackgroundTransparency = 1
+		MeleeG.Size = UDim2.new(1,0,0,0) 
+		MeleeG.LayoutOrder = table.find(RaityLevel,k)
+		MeleeG.AutomaticSize=2
+		MeleeG.LayoutOrder = 100
+		local tvk = Instance.new("UIGridLayout",MeleeG)
+		tvk.CellPadding = UDim2.new(.005,0,.005,0)
+		tvk.CellSize =  UDim2.new(0,70,0,70)
+		tvk.FillDirectionMaxCells = 100
+		tvk.FillDirection = Enum.FillDirection.Horizontal
+
+		local cac = {"Superhuman","ElectricClaw","DragonTalon","SharkmanKarate","DeathStep","GodHuman"}
+		for k,v in pairs(cac) do
+			if ListHuhu[v] and game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("Buy"..v,true) == 1 then 
+				local huhu = Instance.new("ImageLabel",MeleeG)
+				huhu.Image = "rbxassetid://9945562382"
+				huhu.ImageRectSize = Vector2.new(100,100)
+				huhu.ImageRectOffset = ListHuhu[v]*100
+			end
+		end
+		function formatNumber(v)
+			return tostring(v):reverse():gsub("%d%d%d", "%1,"):reverse():gsub("^,", "")
+		end
+		game:GetService("Players").LocalPlayer.PlayerGui.Main.Beli.Position = UDim2.new(0,800,0,500)
+		game:GetService("Players").LocalPlayer.PlayerGui.Main.Level.Position = UDim2.new(0,800,0,550)
+
+		local thieunang = game:GetService("Players").LocalPlayer.PlayerGui.Main.Fragments:Clone()
+		thieunang.Parent = game:GetService("Players").LocalPlayer.PlayerGui.BubbleChat
+		thieunang.Position = UDim2.new(0,800,0.63,0)
+		local n = formatNumber(game.Players.LocalPlayer.Data.Fragments.Value)
+		thieunang.Text = "ƒ"..n
+		print("Done")
+		pcall(function() 
+			game:GetService("Players").LocalPlayer.PlayerGui.Main.MenuButton:Destroy()
+		end)
+		pcall(function() 
+			game:GetService("Players").LocalPlayer.PlayerGui.Main.HP:Destroy()
+		end)
+		pcall(function() 
+			game:GetService("Players").LocalPlayer.PlayerGui.Main.Energy:Destroy()
+		end)
+		for k,v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Main:GetChildren()) do 
+			if v:IsA("ImageButton") then 
+				v:Destroy()
+			end
+		end
+		pcall(function() 
+			game:GetService("Players").LocalPlayer.PlayerGui.Main.Compass:Destroy()
+		end)
+	end,
+
+Misc:Button("Remove cap kaitun",function()
+game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").localPlayer)
+end)      
+      
+Misc:Button("Copy Cframe",function()
+setclipboard('CFrame.new('..tostring(game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame)..')')
+end)      
     
+Misc:Button("Reset Character",function()
+game.Players.localPlayer.Character.Head:Remove()
+end)
     
     Misc:Toggle('Save Member',"6022668898",_G.Setting_table.Save_Member,function(vu)
         _G.Setting_table.Save_Member = vu
@@ -5223,6 +5576,11 @@ spawn(function()
       end
  end)
  
+ 
+ function click()
+ game:GetService'VirtualUser':CaptureController()
+game:GetService'VirtualUser':Button1Down(Vector2.new(1280, 672))
+ end
  
  
   function Bring()
